4a6d858a094cb1d486056a0ddd8868a2ca744c49
metafor <marc@koga.cx>
2017-11-28 16:52:10 +0100

    init

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by metafor on 27.11.17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
